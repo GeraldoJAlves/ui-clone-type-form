32347536392c9f8f901b33b599e5e5014f5d4ca7
Geraldo Alves <junior.alves.2007@gmail.com>
2020-09-19 03:14:18 -0300

    add animation

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4,15 +4,46 @@ import { motion } from 'framer-motion';
 export const Main = styled.main`
 
   .first {
-    background: green;
+    background: red;
     overflow: hidden;
     .offsett{}
+    .a {
+      background: #f5f1ea;
+      height: 100%;
+      display: flex;
+      align-items: flex-end;
+      .left-side {
+        width: 50%;
+        height: 100%;
+        background: #7dbb91;
+      }
+      .rigth-side {
+        width: 50%;
+        height: 100%;
+        .right-image {
+          background: #7dbb91;
+        }
+      }
+    }
+    .b {
+      background: #582524;
+      width: 100%;
+      height: 100%;
+    }
+    .c{
+      background: #0a7397;
+      width: 100%;
+      height: 100%;
+    }
   }
   .second {
-    background: red;
+    background: green;
   }
   .third {
-    background: gray;
+    background: yellow;
+  }
+  .fourth {
+    background: blue;
   }
 `;
 
